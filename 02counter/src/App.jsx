089b721c58596805c484d7ff16c8c5b08117497a
HRUTHIK <hruthik9980819630@gmail.com>
2023-08-25 19:35:34 +0530

    counter app

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(0)
+
+  const addValue = () =>{
+    setCounter(counter+1)
+    if (counter >= 20) {
+      counter = 20;
+      setCounter(counter)
+    }
+  }
+
+  const removeValue = () => {
+    setCounter(counter-1)
+    if (counter <= 0) {
+      counter = 0;
+      setCounter(counter)
+    }
+  }
+
+  const resetValue = () => {
+    counter = 0;
+    setCounter(counter)
+
+  }
+
+
+  return (
+    <>
+    <h1>COFFEE AND REACT</h1>
+    <h2>counter : {counter}</h2>
+    <button onClick={addValue} className='btn'>+</button> <br />
+    <button onClick={removeValue} className='btn'>-</button> <br />
+    <button onClick={resetValue}>RESET</button>
+    </>
+  )
+}
+
+export default App
