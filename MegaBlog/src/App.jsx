159b469ba7e24d603a385552bbe44c0724d0ffe7
HRUTHIK <hruthik9980819630@gmail.com>
2023-10-15 18:12:54 +0530

    appwrite Auth Service

---
+++ MegaBlog/src/App.jsx
@@ -0,0 +1,15 @@
+import './app.css'
+import conf from './config/config';
+
+function App() {
+  console.log(conf.appwriteUrl);
+
+  return (
+    <>
+      <h1>A Blog with appwrite</h1>
+      
+    </>
+  )
+}
+
+export default App
